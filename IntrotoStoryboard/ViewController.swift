ec9f94a7002833c7e507fadd684ecf9dbf08f8c1
Emma Suttell <emsuttell@gmail.com>
2020-08-12 14:35:28 -0700

    Profile page

---
+++ IntrotoStoryboard/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  IntrotoStoryboard
+//
+//  Created by Apple on 8/5/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var aboutMeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
